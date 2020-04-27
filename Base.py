c626ec9f388c439f5bda099c5962d7b93d05357a
ManuMBhat <ManuMBhat>
2020-04-27 19:18:32 +0530

    health Run

--- Base.py
+++ Base.py
@@ -1,12 +1,15 @@
 import numpy as np 
 import random
 import functools
+import pylab
 MAX_GDP = 1000000000000
 random.seed(0)
+deathChance = 0.05
 class Person(object):
     def __init__(self,city,infected):
         self.city = city
         self.infected = infected
+        self.alive = 1
 
     def __str__(self):
         return str(self.infected)
@@ -22,6 +25,9 @@ class Person(object):
 
     def get_infected(self):
         return self.infected
+    
+    def set_alive(self,val):
+        self.alive = val
 
 
 
@@ -35,6 +41,8 @@ class City(object):
         self.people = [Person(self.name,0) for _ in range(self.population)]
         self.avgPeopleContact = 15
         self.transmission = 0.2
+        self.infected = list()
+        self.dead = list()
 
     def __str__(self):
         return self.name
@@ -52,6 +60,13 @@ class City(object):
         self.population += 1
         self.people.append(person)
 
+    def get_travellers(self):
+        return self.travellers
+
+    def social_distancing_protocol(self,socialDistanceVal):
+        self.avgPeopleContact -= (self.avgPeopleContact/socialDistanceVal)
+        self.avgPeopleContact = self.avgPeopleContact//1
+
     def travelling_population(self,restriction = 1):
         self.noOfTravellers = (restriction * self.gdp * self.population)//MAX_GDP
         self.travellers = random.sample(self.people,self.noOfTravellers)
@@ -59,18 +74,37 @@ class City(object):
             self.people.remove(i)
 
     def infection_run(self):
-        infected = list(reduce(lambda x: x.infected == 1 , self.people))
+        
         R0 = self.avgPeopleContact * self.transmission
-        sample = random.sample(self.people,R0*len(infected))
+        sample = random.sample(self.people,R0*len(self.infected))
         for i in sample:
             i.set_infected(1)
+        self.infected = list(reduce(lambda x: x.infected == 1 , self.people))
+
+
+    def health_run(self):
+        for i in self.infected:
+            probabilityOfCure = random.uniform(0,1)
+            if probabilityOfCure <= self.healthCare:
+                i.set_infected(0)
+            else:
+                probabilityOfDeath = random.uniform(0,1)
+                if probabilityOfDeath <= deathChance:
+                    i.set_alive(0)
+                    self.people.remove(i)
+        
+        self.infected = list(reduce(lambda x: x.infected == 1 , self.people))
+
+
+
 
 
 
 
 def test():
     a = City("Milan",10000000,100000000,0.5)     
-    a.travelling_population()  
+    a.travelling_population()
+    print(a.noOfTravellers)
 if __name__ == "__main__":
     test()
     
