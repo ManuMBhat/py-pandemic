1e74d78d84ffcb09ef9fabec1b337e885334012e
mundra-pranay <pranay.mundra@outlook.com>
2020-04-28 19:13:46 +0530

    Restructure base for compatibilty with communityfeats

--- Base.py
+++ Base.py
@@ -1,15 +1,32 @@
 import numpy as np 
 import random
 import functools
+import matplotlib.pyplot as plt
+import math
+
 MAX_GDP = 1000000000000
 random.seed(0)
+deathChance = 0.05
+
+
+"""
+    Helper function
+    Chooses a destination city and adds traveller to that city
+"""
+def travel_to_destination(cities,originCityName,traveller):
+    destination = originCityName
+    while(destination == originCityName):
+        destination = random.choice(cities)
+    destination.add_person(traveller)
+
 class Person(object):
-    def __init__(self,city,infected):
+    state = 'S'
+
+    def __init__(self, city):
         self.city = city
-        self.infected = infected
 
     def __str__(self):
-        return str(self.infected)
+        return str(self.state)
 
     def set_city(self,city):
         self.city = city
@@ -17,16 +34,16 @@ class Person(object):
     def get_city(self):
         return self.city
 
-    def set_infected(self,val):
-        self.infected = val
+    def set_state(self, val):
+        self.state = val
 
-    def get_infected(self):
-        return self.infected
+    def get_state(self):
+        return self.state
 
-
+#integrate community
 
 class City(object):
-    def __init__(self,name,population,gdp,healthCare):
+    def __init__(self, name, population, gdp, healthCare):
         self.name = name
         self.population = population
         self.gdp = gdp
@@ -35,6 +52,8 @@ class City(object):
         self.people = [Person(self.name,0) for _ in range(self.population)]
         self.avgPeopleContact = 15
         self.transmission = 0.2
+        self.infected = list()
+        self.dead = list()
 
     def __str__(self):
         return self.name
@@ -44,36 +63,60 @@ class City(object):
     
     def get_gdp(self):
         return self.gdp
-
+    
+    def get_avgPeopleContact(self):
+        return self.avgPeopleContact
     def get_healthCare(self):
         return self.healthCare
+
+    def get_person(self,index):
+        return self.people[index]
+
+    def get_infected_num(self):
+        return len(self.infected)
+
+    def set_healthCare(self,val):
+        self.healthCare = val
 
     def add_person(self,person):
         self.population += 1
         self.people.append(person)
 
+    def get_travellers(self):
+        return self.travellers
+
+    def social_distancing_protocol(self,socialDistanceVal):
+        self.avgPeopleContact = (1 - socialDistanceVal) * self.avgPeopleContact
+        self.avgPeopleContact = int(self.avgPeopleContact//1)
+
     def travelling_population(self,restriction = 1):
         self.noOfTravellers = (restriction * self.gdp * self.population)//MAX_GDP
         self.travellers = random.sample(self.people,self.noOfTravellers)
+        self.population -= self.noOfTravellers
         for i in self.travellers:
             self.people.remove(i)
 
     def infection_run(self):
-        infected = list(reduce(lambda x: x.infected == 1 , self.people))
-        R0 = self.avgPeopleContact * self.transmission
-        sample = random.sample(self.people,R0*len(infected))
+        
+        R0 = self.avgPeopleContact * self.transmission//1
+        num_infection = int((R0 * len(self.infected))//1)
+        sample = random.sample(self.people,num_infection)
         for i in sample:
             i.set_infected(1)
+        self.infected = list(filter(lambda x: x.infected == 1 , self.people))
 
 
-
-
-def test():
-    a = City("Milan",10000000,100000000,0.5)     
-    a.travelling_population()  
-if __name__ == "__main__":
-    test()
-    
-
-
-    
+    def health_run(self):
+        for i in self.infected:
+            probabilityOfCure = random.SystemRandom().uniform(0,1)
+            if probabilityOfCure <= self.healthCare:
+                i.set_infected(0)
+            else:
+                probabilityOfDeath = random.SystemRandom().uniform(0,1)
+                if probabilityOfDeath <= deathChance:
+                    i.set_alive(0)
+                    i.set_infected(0)
+                    self.people.remove(i)
+                    self.population -=1
+        
+        self.infected = list(filter(lambda x: x.infected == 1, self.people))
--- test.py
+++ test.py
@@ -1,9 +1,33 @@
-class Test(object):
-    def __init__(self,a):
-        self.a = a
+from Base import City,Person
+import matplotlib.pyplot as plt
 
 
-ob = Test(5)
-print(id(ob))
-ob.a = 123456789
-print(id(ob))
+def test():
+    nums = 100
+    output = list()
+    death = list()
+    
+    a = City("Mumbai",1000000,240000000000,0.3)
+    person = a.get_person(10)
+    person.set_infected(1)
+    for i in range(nums):
+        a.infection_run()
+        temp = a.get_population()
+        if i > 6 and i%5 == 1 and a.get_avgPeopleContact() >= 0: 
+            a.set_healthCare(0.7)
+            a.social_distancing_protocol(0.5)
+        a.health_run()
+        output.append(a.get_infected_num())
+        death.append(temp - a.get_population())
+        print(output[-1])
+        if(output[-1] <= 0):
+            break
+    plt.plot(output)
+    plt.plot(death)
+    cumalativeDeath = [sum(death[0:i]) for i in range(len(death))]
+    plt.plot(cumalativeDeath)
+    plt.show()
+        
+if __name__ == "__main__":
+    test()
+    
